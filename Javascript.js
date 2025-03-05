26d95b70b6a039a7bf33e7c56331585ba8846791
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-05 17:45:33 +0000

    Added Java Script Coding

--- Javascript.js
+++ Javascript.js
@@ -4,6 +4,9 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+/**
+ * Displays an alert with the message "Hello, No Answer Data"
+ */
 function myButtonClicked () {
-  alert('Hello, World!')
+  document.getElementById("answer").innerHTML = "Hello, No Answer Data"
 }
